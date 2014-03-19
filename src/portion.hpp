2f805be72bc748faf945ea48ccbe860b32782a03
Leo Osvald <leo.osvald@gmail.com>
2014-03-19 19:30:03 -0400

    Remove warning about inheriting anon ns class. Add AllocPortion and constexpr.

--- src/portion.hpp
+++ src/portion.hpp
@@ -16,10 +16,8 @@ class PortionBase {
   virtual size_t size() const = 0;
 };
 
-namespace {
-
 template<typename T>
-struct SetHelper {
+struct PortionSetHelper {
   template<class P>
   inline void Set(const P& portion, size_t index, const T& value) {
     portion[index] = value;
@@ -27,28 +25,29 @@ struct SetHelper {
 };
 
 template<typename T>
-struct SetHelper<const T> {
+struct PortionSetHelper<const T> {
   template<class P>
   inline void Set(const P& portion, size_t index, const T& value) {}
 };
 
-}  // namespace
-
 template<class InputIter,
          typename T = typename std::iterator_traits<InputIter>::value_type>
-class Portion : public PortionBase<T>, SetHelper<T> {
+class Portion : public PortionBase<T>, protected PortionSetHelper<T> {
  public:
   typedef InputIter Iterator;
-  using DiffType = typename std::iterator_traits<InputIter>::difference_type;
-  using RefType = typename std::iterator_traits<InputIter>::reference;
+  typedef typename std::iterator_traits<InputIter>::difference_type DiffType;
+  typedef typename std::iterator_traits<InputIter>::reference RefType;
 
-  Portion(InputIter begin, DiffType size) : begin_(begin), size_(size) {}
-  Portion(DiffType size, InputIter end) : begin_(end) {
-    std::advance(begin_, -size);
-  }
+  constexpr Portion(const InputIter& begin, DiffType size)
+      : begin_(begin),
+        size_(size) {}
+  constexpr Portion(DiffType size, const InputIter& end)
+      : begin_(std::next(end, -size)),
+        size_(size) {}
+
   Portion() {}
 
-  inline const RefType& operator[](DiffType index) const {
+  constexpr const RefType operator[](DiffType index) const {
     return *std::next(begin_, index);
   }
 
@@ -57,11 +56,13 @@ class Portion : public PortionBase<T>, SetHelper<T> {
   }
 
   inline void set(size_t index, const T& value) {
-    SetHelper<T>::Set(*this, index, value);
+    this->Set(*this, index, value);
   }
 
-  inline const T& get(size_t index) const { return operator[](index); }
-  inline size_t size() const { return static_cast<decltype(size())>(size_); }
+  constexpr const T& get(size_t index) const { return operator[](index); }
+  constexpr size_t size() const {
+    return static_cast<decltype(size())>(size_);
+  }
 
  private:
   InputIter begin_;
@@ -86,19 +87,37 @@ using IsReadOnly = std::is_const<
 template<class InputIter>
 typename std::enable_if<
   !IsReadOnly<InputIter>::value,
-  Portion<InputIter> >::type MakePortion(
-        InputIter begin,
-        typename Portion<InputIter>::DiffType size) {
+  Portion<InputIter> >::type constexpr MakePortion(
+      InputIter begin,
+      typename Portion<InputIter>::DiffType size) {
   return Portion<InputIter>(begin, size);
 }
 
 template<class InputIter>
 typename std::enable_if<
   IsReadOnly<InputIter>::value,
-  ReadonlyPortion<InputIter> >::type MakePortion(
+  ReadonlyPortion<InputIter> >::type constexpr MakePortion(
       InputIter begin,
       typename ReadonlyPortion<InputIter>::DiffType size) {
   return ReadonlyPortion<InputIter>(begin, size);
+}
+
+template<class InputIter>
+typename std::enable_if<
+  !IsReadOnly<InputIter>::value,
+  Portion<InputIter>* >::type constexpr AllocPortion(
+      InputIter begin,
+      typename Portion<InputIter>::DiffType size) {
+  return new Portion<InputIter>(begin, size);
+}
+
+template<class InputIter>
+typename std::enable_if<
+  IsReadOnly<InputIter>::value,
+  ReadonlyPortion<InputIter>* >::type constexpr AllocPortion(
+      InputIter begin,
+      typename ReadonlyPortion<InputIter>::DiffType size) {
+  return new ReadonlyPortion<InputIter>(begin, size);
 }
 
 }  // namespace v
