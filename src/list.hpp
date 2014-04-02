7fc8ba00b11fff90bf997083dd73333d04bc5e5a
Leo Osvald <leo.osvald@gmail.com>
2014-04-02 12:24:31 -0400

    Implement random-access of List View using a skip list without finger/offset.

--- src/list.hpp
+++ src/list.hpp
@@ -4,6 +4,7 @@
 #include "portion.hpp"
 #include "portion_helper.hpp"
 #include "view.hpp"
+#include "util/immutable_skip_list.hpp"
 #include "util/poly_vector.hpp"
 
 #include <vector>
@@ -18,27 +19,60 @@ namespace {
 template<class PolyContainer,
          typename T = typename PolyContainer::Pointer::element_type::ValueType>
 struct LinearIndexer {
-  const T& operator()(const PolyContainer& portions, size_t index) const {
-    size_t cum_size = 0, i_to = portions.size();
+  LinearIndexer(const PolyContainer& portions) : portions_(portions) {}
+
+  const T& operator()(size_t index) const {
+    size_t cum_size = 0, i_to = portions_.size();
     for (size_t i = 0; i < i_to; ++i) {
-      const auto& p = portions[i];
+      const auto& p = portions_[i];
       if ((cum_size += p.size()) > index)
         return p.get(index - (cum_size - p.size()));
     }
-    return portions[i_to - 1].get(portions[i_to - 1].size()); // not found
+    return portions_[i_to - 1].get(portions_[i_to - 1].size()); // not found
   }
+
+ private:
+  const PolyContainer& portions_;
+};
+
+template<class PolyContainer,
+         typename T = typename PolyContainer::Pointer::element_type::ValueType>
+struct LogarithmicIndexer {
+  LogarithmicIndexer(const PolyContainer& portions) :
+      bsg_(portions),
+      fwd_(portions.size(), bsg_) {
+  }
+
+  inline const T& operator()(size_t index) const {
+    auto pos = fwd_.get(index);
+    return bsg_.portions_[pos.first].get(pos.second);
+  }
+
+ private:
+  struct BucketSizeGetter {
+    const PolyContainer& portions_;
+    BucketSizeGetter(const PolyContainer& portions) : portions_(portions) {}
+    inline size_t operator()(size_t bucket_index) const {
+      return portions_[bucket_index].size();
+    }
+  } bsg_;
+  ImmutableSkipList<BucketSizeGetter> fwd_;
+  // TODO optimize short jumps (need also a "backard" skip list)
+  // ImmutableSkipList<ReverseBucketSizeGetter> bwd_;
 };
 
 }  // namespace
 
-template<typename T, class P = PortionBase<T> >
+template<typename T, class P = PortionBase<T>,
+         class Indexer = LogarithmicIndexer<PortionVector<T, P> > >
 class List : public View<T>, protected PortionHelper<P, T> {
  public:
   typedef typename PortionVector<T, P>::Pointer PortionPointer;
 
   List() {}
   List(PortionVector<T, P>&& pv)
-      : portions_(std::forward<PortionVector<T, P> >(pv)) {
+      : portions_(std::forward<PortionVector<T, P> >(pv)),
+        indexer_(portions_) {
     portions_.Shrink();
   }
 
@@ -47,25 +81,20 @@ class List : public View<T>, protected PortionHelper<P, T> {
   }
 
   inline const T& get(size_t index) const {
-    return indexer_(portions_, index);
+    return indexer_(index);
   }
 
   size_t max_size() const { return portions_.max_size(); }
 
  protected:
+
+  // ImmutableSkipList<BucketSizeGetter> positions_;
+
   PortionVector<T, P> portions_;
-  LinearIndexer<decltype(portions_)> indexer_;
+  Indexer indexer_;
+  // LinearIndexer<decltype(portions_)> indexer_;
 
  private:
-  // T& get_linear(size_t index) const {
-  //   size_t cum_size = 0, i_to = portions_.size();
-  //   for (size_t i = 0; i < i_to; ++i) {
-  //     const P& p = portions_[i];
-  //     if ((cum_size += p.size()) > index)
-  //       return p.get(index - (cum_size - p.size()));
-  //   }
-  //   return portions_[i_to - 1].get(portions_[i_to - 1].size());
-  // }
 };
 
 template<typename T, class P>
