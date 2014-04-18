5f9efa42024fea555bc987f566d7071ec222111a
Leo Osvald <leo.osvald@gmail.com>
2014-04-18 15:18:55 -0400

    Implement resizable ChunkedArray as a specializtion for size=0 (default). Swap kChunkSizeLog and kSize template parameters, respectively.

--- src/util/chunked_array.hpp
+++ src/util/chunked_array.hpp
@@ -1,9 +1,10 @@
 #ifndef CPPVIEWS_SRC_UTIL_CHUNKED_ARRAY_HPP_
 #define CPPVIEWS_SRC_UTIL_CHUNKED_ARRAY_HPP_
 
+#include <algorithm>
 #include <array>
 
-template<typename T, size_t kSize, unsigned kChunkSizeLog,
+template<typename T, unsigned kChunkSizeLog, size_t kSize = 0,
          class Alloc = std::allocator<T> >
 class ChunkedArray {
  public:
@@ -47,6 +48,59 @@ class ChunkedArray {
   Alloc alloc_;
 
   std::array<T*, kChunkCount> chunks_;
+};
+
+// specialize for case of runtime size (0)
+template<typename T, unsigned kChunkSizeLog, class Alloc>
+class ChunkedArray<T, kChunkSizeLog, 0, Alloc> {
+ public:
+  static constexpr size_t kChunkSize = size_t(1) << kChunkSizeLog;
+
+  ChunkedArray() : chunks_(NULL), chunk_count_(0) {}
+  ChunkedArray(size_t chunk_count) {
+    this->Resize(chunk_count);
+  }
+
+  ~ChunkedArray() {
+    this->DeallocateChunks(0);
+    delete[] chunks_;
+  }
+
+  const T& operator[](size_t index) const {
+    return chunks_[index >> kChunkSizeLog][index & (kChunkSize - 1)];
+  }
+
+  T& operator[](size_t index) {
+    return const_cast<T&>((*static_cast<const ChunkedArray*>(this))[index]);
+  }
+
+  void Fill(const T& value) {
+    for (decltype(chunk_count_) i = 0; i < chunk_count_; ++i)
+      std::fill(chunks_[i], chunks_[i] + kChunkSize, value);
+  }
+
+  void Resize(size_t chunk_count) {
+    this->DeallocateChunks(chunk_count);
+    T** chunks = new T*[chunk_count];
+    for (auto i = chunk_count_; i < chunk_count; ++i)
+      chunks[i] = alloc_.allocate(kChunkSize);
+    std::copy_n(chunks_, std::min(chunk_count_, chunk_count), chunks);
+    delete[] chunks_;
+    chunks_ = chunks;
+    chunk_count_ = chunk_count;
+  }
+
+  size_t size() const { return chunk_count_ << kChunkSizeLog; }
+
+ private:
+  void DeallocateChunks(size_t offset) {
+    for (auto i = offset; i < chunk_count_; ++i)
+      alloc_.deallocate(chunks_[i], kChunkSize);
+  }
+
+  Alloc alloc_;
+  T** chunks_;
+  size_t chunk_count_;
 };
 
 #endif  /* CPPVIEWS_SRC_UTIL_CHUNKED_ARRAY_HPP_ */
--- test/util/chunked_array_test.cpp
+++ test/util/chunked_array_test.cpp
@@ -20,21 +20,75 @@ typedef ::testing::Types<
   > ChunkSizeLog;
 TYPED_TEST_CASE(ChunkedArrayTest, ChunkSizeLog);
 
-TYPED_TEST(ChunkedArrayTest, ReadWrite) {
-  ChunkedArray<int, 100, TypeParam::value> a;
+TYPED_TEST(ChunkedArrayTest, ReadWriteFixed) {
+  ChunkedArray<int, TypeParam::value, 100> a;
   ASSERT_EQ(100, a.size());
   for (int i = 0; i < a.size(); ++i)
     ASSERT_EQ(i + 100, a[i] = i + 100) << "Failed read/write at index " << i;
 
-  ChunkedArray<int, 1, TypeParam::value> singleton;
+  ChunkedArray<int, TypeParam::value, 1> singleton;
   EXPECT_EQ(1, singleton.size());
   EXPECT_EQ(42, singleton[0] = 42);
 }
 
-TYPED_TEST(ChunkedArrayTest, Fill) {
-  ChunkedArray<int, 100, TypeParam::value> a;
+TYPED_TEST(ChunkedArrayTest, FillFixed) {
+  ChunkedArray<int, TypeParam::value, 100> a;
   a.Fill(13);
   ASSERT_EQ(100, a.size());
   for (int i = 0; i < a.size(); ++i)
     ASSERT_EQ(13, a[i]);
+}
+
+TYPED_TEST(ChunkedArrayTest, ReadWriteResizeable) {
+  constexpr auto kChunkSizeLog = TypeParam::value;
+  ChunkedArray<int, kChunkSizeLog> a;
+  a.Resize(2);
+  ASSERT_EQ(2 << kChunkSizeLog, a.size());
+  EXPECT_EQ(-3, a[0] = -3);
+  EXPECT_EQ(-17, a[1] = -17);
+
+  // expand
+  a.Resize(5);
+  ASSERT_EQ(5 << kChunkSizeLog, a.size());
+  EXPECT_EQ(15, a[2] = 15);
+  EXPECT_EQ(-17, a[1]);  // verify not overwritten
+  EXPECT_EQ(-1, a[4] = -1);
+
+  // shrink
+  a.Resize(1);
+  EXPECT_EQ(-3, a[0]);  // verify not overwritten
+  EXPECT_EQ(10, a[0] = 10);  // check overwriting works
+
+  // expand it again
+  a.Resize(100);
+  ASSERT_EQ(100 << kChunkSizeLog, a.size());
+  for (int i = 0; i < a.size(); ++i)
+    ASSERT_EQ(i + 100, a[i] = i + 100) << "Failed read/write at index " << i;
+}
+
+TYPED_TEST(ChunkedArrayTest, FillResizeable) {
+  constexpr auto kChunkSizeLog = TypeParam::value;
+  ChunkedArray<int, kChunkSizeLog> a;
+  a.Fill(-1);  // verify robustness by filling empty array
+
+  a.Resize(100);
+  a.Fill(13);
+  ASSERT_EQ(100 << kChunkSizeLog, a.size());
+  for (int i = 0; i < a.size(); ++i)
+    ASSERT_EQ(13, a[i]);
+
+  // shrink it
+  a.Resize(10);
+  for (int i = 0; i < 10; ++i)
+    ASSERT_EQ(13, a[i]);
+
+  // expand it again
+  a.Resize(73);
+  ASSERT_EQ(73 << kChunkSizeLog, a.size());
+  for (int i = 0; i < 10; ++i)
+    ASSERT_EQ(13, a[i]);
+
+  a.Fill(5);
+  for (int i = 0; i < a.size(); ++i)
+    ASSERT_EQ(5, a[i]);
 }
