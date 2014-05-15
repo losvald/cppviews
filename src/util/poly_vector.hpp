9d991c33ba21d35afd23ad797f81ede09bb2e918
Leo Osvald <leo.osvald@gmail.com>
2014-05-15 19:15:36 -0400

    Support iteration on polymorphic vector and allow for polymorphic factory.

--- src/util/poly_vector.hpp
+++ src/util/poly_vector.hpp
@@ -2,10 +2,13 @@
 #define CPPVIEWS_SRC_UTIL_POLY_VECTOR_HPP_
 
 #include "fake_pointer.hpp"
+#include "iterator.hpp"
 
 #include <memory>
 #include <type_traits>
 #include <vector>
+
+namespace detail {
 
 template<class T, class Base = T>
 using PolyUniqPtr = typename std::conditional<
@@ -24,28 +27,65 @@ struct PolyVectorEmplaceHelper {
 };
 
 template<typename T>
+struct PolyVectorEmplaceHelper<T, T> {
+  template<class Derived, typename... Args>
+  inline void Emplace(
+      std::vector<PolyUniqPtr<T, T> >& v, Args&&... args) const {
+    // TODO: optimize using placement new (to ensure locality of pointed data)
+    v.emplace_back(new Derived(std::forward<Args>(args)...));
+  }
+};
+
+template<typename T>
 struct PolyVectorDefaultFactory {
   template<typename... Args>
   constexpr T operator()(Args&&... args) const { return T(); }
 };
 
-template<class T, class Base = T, class Factory = PolyVectorDefaultFactory<T> >
-class PolyVector {
- public:
-  typedef PolyUniqPtr<T, Base> Pointer;
+template<class T, class Base, class Factory>
+struct PolyVectorFactoryProvider {
+  typedef typename std::conditional<
+    std::is_same<T, Base>::value,
+    Factory,
+    PolyVectorDefaultFactory<T> >::type FactoryType;
+};
 
-  PolyVector(size_t capacity) { v_.reserve(capacity); }
+}  // namespace detail
+
+template<class T, class Base = T,
+         class Factory = detail::PolyVectorDefaultFactory<T> >
+class PolyVector {
+  // for convenience, allow user to always specify a polymorphic factory,
+  // so use default (which generates derived) in the non-poly case
+  typedef typename detail::PolyVectorFactoryProvider<T, Base, Factory>
+  ::FactoryType FactoryType;
+  typedef detail::PolyUniqPtr<T, Base> Pointer;
+  typedef std::vector<Pointer> WrappedType;
+
+  struct IteratorTransformer;
+  struct ConstIteratorTransformer;
+
+ public:
+  typedef typename Pointer::element_type ValueType;
+  typedef ValueType& Ref;
+  typedef const Ref ConstRef;
+  typedef typename WrappedType::size_type SizeType;
+
+  typedef TransformedIterator<typename WrappedType::iterator,
+                              IteratorTransformer
+                              > Iterator;
+  typedef TransformedIterator<typename WrappedType::const_iterator,
+                              ConstIteratorTransformer,
+                              typename WrappedType::iterator
+                              > ConstIterator;
+
+  PolyVector(SizeType capacity) { v_.reserve(capacity); }
   // PolyVector(const PolyVector& pv) = default;
   // PolyVector(PolyVector&& pv) = default;
   PolyVector() = default;
 
-  inline const T& operator[](size_t index) const {
-    return *v_[index];
-  }
-
-  inline T& operator[](size_t index) {
-    return *v_[index];
-  }
+  inline ConstRef operator[](SizeType index) const { return *v_[index]; }
+  inline Ref operator[](SizeType index) { return *v_[index]; }
 
   template<typename... Args>
   inline PolyVector& AppendThis(Args&&... args) {
@@ -54,34 +94,39 @@ class PolyVector {
 
   template<typename... Args>
   inline PolyVector&& Append(Args&&... args) {
-    return this->Append<decltype(Factory()(std::forward<Args>(args)...))>(
+    return this->Append<decltype(FactoryType()(std::forward<Args>(args)...))>(
         std::forward<Args>(args)...);
   }
 
   template<class Derived, typename... Args>
   inline PolyVector&& Append(Args&&... args) {
-    PolyVectorEmplaceHelper<T, Base> h;
+    detail::PolyVectorEmplaceHelper<T, Base> h;
     h.template Emplace<Derived>(v_, std::forward<Args>(args)...);
     return std::move(*this);
   }
 
   void Shrink() { v_.shrink_to_fit(); }
 
-  size_t size() const { return v_.size(); }
-  size_t max_size() const { return v_.max_size(); }
+  Iterator begin() { return v_.begin(); }
+  Iterator end() { return v_.end(); }
+  ConstIterator begin() const { return v_.begin(); }
+  ConstIterator end() const { return v_.end(); }
+  ConstIterator cbegin() const { return v_.cbegin(); }
+  ConstIterator cend() const { return v_.cend(); }
+
+  SizeType size() const { return v_.size(); }
+  SizeType max_size() const { return v_.max_size(); }
 
  private:
-  std::vector<Pointer> v_;
-};
+  struct IteratorTransformer {
+    Ref operator()(Pointer& ptr) const { return *ptr; }
+  };
 
-template<typename T>
-struct PolyVectorEmplaceHelper<T, T> {
-  template<class Derived, typename... Args>
-  inline void Emplace(
-      std::vector<PolyUniqPtr<T, T> >& v, Args&&... args) const {
-    // TODO: optimize using placement new (to ensure locality of pointed data)
-    v.emplace_back(new Derived(std::forward<Args>(args)...));
-  }
+  struct ConstIteratorTransformer {
+    ConstRef operator()(const Pointer& ptr) const { return *ptr; }
+  };
+
+  WrappedType v_;
 };
 
 #endif  /* CPPVIEWS_SRC_UTIL_POLY_VECTOR_HPP_ */
