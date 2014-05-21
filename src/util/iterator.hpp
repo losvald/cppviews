c82bb3c1b0c899f77b0ea6ab24e831a6e7e240b1
Leo Osvald <leo.osvald@gmail.com>
2014-05-20 21:40:24 -0400

    Implement default iterator facade (inspired by Boost) with interoperability. This should allow easy implementation of poly-container and view iterators.

--- src/util/iterator.hpp
+++ src/util/iterator.hpp
@@ -4,7 +4,17 @@
 #include <iterator>
 #include <type_traits>
 
+#include <cstddef>
+
 namespace detail {
+
+constexpr bool Or2(bool cond1, bool cond2) { return cond1 || cond2; }
+
+template<class Iter1, class Iter2, class Return>
+using EnableIfInteroperable = typename std::enable_if<
+  Or2(std::is_convertible<Iter1, Iter2>::value,
+      std::is_convertible<Iter2, Iter1>::value),
+  Return>::type;
 
 template<class InputIter>
 struct InputIterHelper {
@@ -76,6 +86,19 @@ public:
 
 }  // namespace detail
 
+template<class Iter>
+using IsConstIter = std::is_const<
+  typename std::remove_pointer<
+    typename std::remove_cv<
+      typename std::iterator_traits<Iter>::pointer
+      >::type
+    >::type
+  >;
+
+template<class FromValue, class ToValue, class Return>
+using EnableIfIterConvertible = typename std::enable_if<
+  std::is_convertible<FromValue*, ToValue*>::value, Return>::type;
+
 template<class InputIter, class Func, class NonConstInputIter = InputIter>
 #define V_THIS_TYPE TransformedIterator<InputIter, Func>
 #define V_THIS_REF_TYPE detail::TransformedIterRef<InputIter, Func>
@@ -122,5 +145,154 @@ using IndirectIterator = TransformedIterator<
   InputIter,
   detail::IndirectIterTransformer<InputIter>,
   NonConstInputIter>;
+
+// Adapter which adapts the following simplified interface:
+// V& ref() const;  // dereferences the iterator
+// for forward iterators:
+//   void Increment();
+//   template<class...> IsEqual(const Iterator<...>& other) const;
+// for bidirectional iterators:
+//   void Decrement();
+// for random-access iterators:
+//   void Advance(Distance n);
+//   template<class...> Distance DistanceTo(const Iterator<...>& other) const;
+//
+// Typical usage is like this:
+// template<class T>
+// class Iterator : public DefaultIterator<Iterator,
+//                                         std::forward_iterator_tag,
+//                                         T> {
+//   V_DEFAULT_ITERATOR_DERIVED_HEAD(Iterator);
+//  public:
+//   Iterator(/* main ctor */);
+//  protected:
+//   void Increment();
+//   template<class T2>
+//   bool IsEqual(const Iterator<T2>& other,
+//                EnableIfIterConvertible<T2, T, Enabler> = Enabler()) const;
+//   T& ref() const;
+//   ...
+// };
+template<class Derived,
+         class Category,
+         class V,
+         class Distance = std::ptrdiff_t>
+struct DefaultIterator : public std::iterator<Category, V> {
+
+#define V_DEFAULT_ITERATOR_DERIVED_HEAD(Iter)                           \
+  typedef typename Iter::DefaultIterator_ DefaultIterator_;             \
+  typedef typename DefaultIterator_::Enabler Enabler;                   \
+  template<class, class, class, class> friend class DefaultIterator;
+
+#define V_DEFAULT_ITERATOR_DISABLE_NONCONVERTIBLE(FromValue)            \
+  typename DefaultIterator_::template                                   \
+  EnablerIfConvertibleFrom<T2>::type = typename DefaultIterator_::Enabler()
+
+ public:
+  typedef DefaultIterator<Derived, Category, V> DefaultIterator_;
+
+  V& operator*() const {
+    return this->derived().ref();
+  }
+  V* operator->() const {
+    return &*this->operator();
+  }
+
+  Derived& operator++() {
+    this->derived().Increment();
+    return this->derived();
+  }
+  Derived operator++(int) {
+    Derived old(this->derived());
+    ++this->derived();
+    return old;
+  }
+
+  template<class Derived2>
+  detail::EnableIfInteroperable<Derived, Derived2, bool>
+  operator==(const Derived2& rhs) const {
+    return this->derived().IsEqual(rhs);
+  }
+
+  template<class Derived2>
+  detail::EnableIfInteroperable<Derived, Derived2, bool>
+  operator!=(const Derived2& rhs) const {
+    return !this->derived().IsEqual(rhs);
+  }
+
+ protected:
+  struct Enabler {};
+  template<typename T2>
+  using EnablerIfConvertibleFrom = typename std::enable_if<
+    std::is_convertible<T2*, V*>::value,
+    Enabler>;
+
+  Derived& derived() { return *static_cast<Derived*>(this); }
+  const Derived& derived() const { return *static_cast<const Derived*>(this); }
+};
+
+template<class Derived, class V, typename Distance>
+class DefaultIterator<Derived, std::bidirectional_iterator_tag, V, Distance>
+    : public DefaultIterator<Derived, std::forward_iterator_tag, V, Distance> {
+ public:
+  Derived& operator--() {
+    this->derived().Decrement();
+    return this->derived();
+  }
+  Derived operator--(int) {
+    Derived old(this->derived());
+    --this->derived();
+    return old;
+  }
+};
+
+template<class Derived, class V, typename Distance>
+class DefaultIterator<Derived, std::random_access_iterator_tag, V, Distance>
+    : public DefaultIterator<Derived, std::bidirectional_iterator_tag,
+                             V, Distance> {
+ public:
+  Derived& operator+=(Distance n) {
+    this->derived().Advance(n);
+    return this->derived();
+  }
+  Derived& operator-=(Distance n) { return *this += -n; }
+  Derived operator+(Distance n) const {
+    Derived it(this->derived());
+    it.Advance(n);
+    return it;
+  }
+  Derived operator-(Distance n) const { return *this + -n; }
+
+  template<class Derived2>
+  detail::EnableIfInteroperable<Derived, Derived2, Distance>
+  operator-(const Derived2& rhs) const {
+    return rhs.DistanceTo(this->derived());
+  }
+
+  // define relations <, <=, >, >=
+#define V_DEF_ITERATOR_INTEROP_RELATION(rel)                          \
+  template<class Derived2>                                            \
+  detail::EnableIfInteroperable<Derived, Derived2, bool>              \
+  operator rel(const Derived2& rhs) const {                           \
+    return 0 rel this->derived().DistanceTo(rhs);                     \
+  }
+
+  V_DEF_ITERATOR_INTEROP_RELATION(<);
+  V_DEF_ITERATOR_INTEROP_RELATION(<=);
+  V_DEF_ITERATOR_INTEROP_RELATION(>);
+  V_DEF_ITERATOR_INTEROP_RELATION(>=);
+#undef V_DEF_ITERATOR_INTEROP_RELATION
+
+  friend Derived operator+(Distance n, const Derived& rhs) { return rhs + n; }
+  friend Derived operator-(Distance n, const Derived& rhs) { return rhs - n; }
+
+ protected:
+  // provide default implementation of Increment()/Decrement() using Advance()
+  // the derived class can, however, override them with a more efficient impl
+  void Increment() { this->derived().Advance(1); }
+  void Decrement() { this->derived().Advance(-1); }
+
+  template<class, class, class, class> friend class DefaultIterator;
+};
 
 #endif  /* CPPVIEWS_SRC_UTIL_ITERATOR_HPP_ */
--- test/util/iterator_test.cpp
+++ test/util/iterator_test.cpp
@@ -1,6 +1,8 @@
 #include "../../src/util/iterator.hpp"
 #include "../test.hpp"
 
+#include <initializer_list>
+#include <list>
 #include <vector>
 
 #include <cctype>
@@ -92,7 +94,7 @@ struct Minus1Transformer {
 };
 
 
-TEST(IteratorTest, CustomProxy) {
+TEST(IteratorTest, TransformedCustomProxy) {
   typedef TransformedIterator<int*, Minus1Transformer<int> > TI;
   int x = 5;
   TI ti = &x;
@@ -111,12 +113,12 @@ TEST(IteratorTest, CustomProxy) {
   CTI cti = ti;
   EXPECT_EQ(9, *cti);
   cti = &x;  // validate construction from non-transformed const iter
-  *cti = 10;
+  *cti++ = 10;
   EXPECT_EQ(9, *ti);
   // ti = cti;  // compile error - OK
 }
 
-TEST(IteratorTest, Const) {
+TEST(IteratorTest, TransformedConst) {
   struct Iter : public std::iterator<std::forward_iterator_tag,
                                     const char> {
     Iter() = default;
@@ -147,4 +149,194 @@ TEST(IteratorTest, Const) {
   EXPECT_EQ('F', *vti);
   EXPECT_EQ('f', v.front());  // validate the vector is unmodified
   // *vti = 'a';  // compile error - OK
+}
+
+template<typename T>
+class EverySecondArrayIter
+    : public DefaultIterator<EverySecondArrayIter<T>,
+                             std::random_access_iterator_tag, T> {
+  V_DEFAULT_ITERATOR_DERIVED_HEAD(EverySecondArrayIter);
+
+ public:
+  EverySecondArrayIter(T* arr) : cur_(arr) {}
+
+  template<typename T2>
+  EverySecondArrayIter(const EverySecondArrayIter<T2>& other,
+                       EnableIfIterConvertible<T2, T, Enabler> = Enabler())
+      : cur_(other.cur_) {}
+
+ protected:
+  void Advance(int n) {
+    cur_ += 2 * n;
+  }
+  template<typename T2>
+  typename DefaultIterator_::difference_type
+  DistanceTo(const EverySecondArrayIter<T2>& to) const {
+    return (to.cur_ - cur_) / 2;
+  }
+  template<typename T2>
+  bool IsEqual(const EverySecondArrayIter<T2>& other) const {
+    return cur_ == other.cur_;
+  }
+  T& ref() const { return *cur_; }
+
+ private:
+  template<class> friend class EverySecondArrayIter;
+  T* cur_;
+};
+
+TEST(IteratorTest, DefaultRandomAccess) {
+  std::array<int, 5> a{ {10, 20, 30, 40, 50} };
+  typedef EverySecondArrayIter<int> ESI;
+
+  ESI esi(&a.front());
+  EXPECT_EQ(10, *esi);
+  EXPECT_EQ(30, *++esi);
+  EXPECT_EQ(30, *esi++);
+  EXPECT_EQ(50, *esi);
+
+  // validate modification through iterator are transient
+  *esi = 500;
+  EXPECT_EQ(500, a[4]);
+  *esi = 50;
+  EXPECT_EQ(50, a[4]);
+
+  // validate forward iterator operations
+  esi = ESI(&a.front());
+  ASSERT_EQ(esi, ESI(&a.front()));
+  ASSERT_FALSE(esi != ESI(&a.front()));
+  ASSERT_NE(esi, ESI(&a.back()));
+  EXPECT_EQ(10, *esi++);
+  EXPECT_EQ(30, *esi);
+  EXPECT_EQ(50, *++esi);
+  EXPECT_EQ(esi, ESI(&a.back()));
+
+  // validate bidirectional iterator operations
+  EXPECT_EQ(50, *esi--);
+  EXPECT_NE(esi, ESI(&a.back()));
+  EXPECT_EQ(30, *esi);
+  EXPECT_EQ(10, *--esi);
+  EXPECT_EQ(esi, ESI(&a.front()));
+
+  // validate random access iterator operations
+  esi = &a.front();
+  EXPECT_LT(esi, esi + 1);
+  EXPECT_FALSE(esi < esi);
+  EXPECT_LE(esi, esi);
+  EXPECT_LE(esi, esi + 1);
+  EXPECT_FALSE(esi + 1 <= esi);
+  EXPECT_GT(esi + 1, esi);
+  EXPECT_FALSE(esi > esi);
+  EXPECT_GE(esi, esi);
+  EXPECT_GE(esi + 1, esi);
+  EXPECT_FALSE(esi >= esi + 1);
+  EXPECT_EQ(50, *(esi += 2));
+  EXPECT_EQ(10, *(esi -= 2));
+  EXPECT_EQ(50, *(ESI(&a.front()) + 2));
+  EXPECT_EQ(10, *(ESI(&a.back()) - 2));
+  EXPECT_EQ(50, *(2 + ESI(&a.front())));
+  EXPECT_EQ(10, *(-2 + ESI(&a.back())));
+  EXPECT_EQ(10, *(-2 + ESI(&a.back())));
+  EXPECT_EQ(ESI(&a.back()) - ESI(&a.front()), 2);
+  EXPECT_EQ(ESI(&a.front()) - ESI(&a.front() + 2), -1);
+
+  // validate non-const to const conversion and some interoperability
+  typedef EverySecondArrayIter<const int> CESI;
+  static_assert(std::is_convertible<ESI, CESI>::value, "non-const -> const");
+  static_assert(!std::is_convertible<CESI, ESI>::value, "! const -> non-const");
+
+  // EverySecondArrayIt<char> cesi2 = static_cast<char*>(
+  //     static_cast<void*>(a));
+  // EXPECT_NE(cesi2, esi);  // compile error - OK: no match for operator!=
+  // EXPECT_NE(esi, cesi2);  // compile error - OK: no match for operator!=
+
+  CESI cesi(&a.front());
+  esi = ESI(&a.back());
+  EXPECT_NE(cesi, esi);
+  EXPECT_NE(esi, cesi);
+  ++cesi;
+  EXPECT_EQ(30, *cesi);
+  cesi = esi;
+  ASSERT_EQ(50, *cesi);
+  EXPECT_EQ(cesi, esi);
+  EXPECT_EQ(esi, cesi);
+
+  // validate more interoperability
+  esi = &a.front();
+  cesi = &a.front();
+  EXPECT_LT(cesi, esi + 1);
+  EXPECT_LT(esi, cesi + 1);
+  EXPECT_LE(esi, cesi);
+  EXPECT_LE(cesi, esi);
+  EXPECT_GT(1 + esi, cesi);
+  EXPECT_GT(1 + cesi, esi);
+  EXPECT_GE(esi, cesi);
+  EXPECT_GE(cesi, esi);
+  EXPECT_EQ(2, ESI(&a.back()) - cesi);
+  EXPECT_EQ(2, CESI(&a.back()) - esi);
+}
+
+// it is also possible to use DefaultIter with inner classes, but it's messy
+
+template<typename T>
+class PointerList {
+ public:
+  class Iterator : public DefaultIterator<
+    Iterator,
+    std::forward_iterator_tag, T> {
+    V_DEFAULT_ITERATOR_DERIVED_HEAD(Iterator);
+
+    typedef typename std::conditional<
+      std::is_const<T>::value,
+      typename std::list<typename std::remove_cv<T>::type*>::const_iterator,
+      typename std::list<T*>::iterator>::type InnerIter;
+    InnerIter cur_;
+   public:
+    Iterator(InnerIter cur) : cur_(cur) {}
+    Iterator(const Iterator& other) : cur_(other.cur_) {}
+    // TODO: support non-const to const conversion
+    // (PointerList<...>::Iterator fails because of incomplete outer class)
+   protected:
+    void Increment() { ++cur_; }
+    bool IsEqual(const Iterator& other) const { return cur_ == other.cur_; }
+    T& ref() const { return **cur_; }
+  };
+  typedef typename PointerList<const T>::Iterator ConstIterator;
+
+  PointerList(std::initializer_list<T*> init_list) : list_(init_list) {}
+  Iterator begin() { return Iterator(list_.begin()); }
+  Iterator end() { return Iterator(list_.end()); }
+  ConstIterator cbegin() { return ConstIterator(list_.cbegin()); }
+  ConstIterator cend() { return ConstIterator(list_.cend()); }
+
+ private:
+  std::list<T*> list_;
+};
+
+TEST(IteratorTest, DefaultForward) {
+  int x = 10, y = 20, z = 30;
+  PointerList<int> l({&x, &y, &z});
+
+  // validate forward iterator operations
+  typedef decltype(l.begin()) PLI;
+  PLI pli(l.begin());
+  ASSERT_EQ(pli, PLI(l.begin()));
+  ASSERT_FALSE(pli != PLI(l.begin()));
+  ASSERT_NE(pli, PLI(l.end()));
+  EXPECT_EQ(10, *pli++);
+  EXPECT_EQ(20, *pli);
+  EXPECT_EQ(30, *++pli);
+  EXPECT_EQ(++pli, PLI(l.end()));
+
+  // validate change via reference
+  typedef decltype(l.cbegin()) CPLI;
+  typedef std::iterator_traits<PLI>::reference PLIRef;
+  PLIRef r = *PLI(l.begin());
+  CPLI cpli = l.cbegin();
+  pli = l.begin();
+  r = 100;
+  EXPECT_EQ(100, *pli);
+  EXPECT_EQ(100, *cpli); // validate the change is seen by the const iterator
+
+  // TODO: validate non-const to const conversion
 }
