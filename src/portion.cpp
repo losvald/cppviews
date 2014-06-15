f80a3a68b2b50c6c46236b776c3d5302d570d4f2
Leo Osvald <leo.osvald@gmail.com>
2014-06-15 12:03:24 -0400

    Make the library header-only (remove an unused incomplete file)

--- src/portion.cpp
+++
@@ -1,5 +0,0 @@
-#include "portion.hpp"
-
-Portion::Portion(void* begin, size_t size)  : begin_(begin), size_(size) {}
-
-Portion::Portion(size_t size, void* end)  : begin_(end - size), size_(size) {}
