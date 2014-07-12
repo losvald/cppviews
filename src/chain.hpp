54e5ded62c79a25e2cb513f12387a4e7d2f88f5f
Leo Osvald <leo.osvald@gmail.com>
2014-07-12 05:19:35 -0400

    Implement Random Access via get(SizeArray) for the Chain view.

--- src/chain.hpp
+++ src/chain.hpp
@@ -24,6 +24,29 @@ class ChainedListVector : public ListVector<SublistType> {
   ChainedListVector() = default;
 };
 
+template<class L>
+struct ChainHelper {
+  template<class Container>
+  static void InsertDummies(Container* c) {
+    // add a sentinel list at the front (copy first and shrink it to size 0)
+    c->Add(c->begin(), c->front());
+    c->front().ShrinkToFirst();
+
+    // add a sentinel list at the back (copy last and shrink it to size 0)
+    c->Append(c->back());
+    c->back().ShrinkToFirst();
+  }
+};
+
+template<typename T, unsigned dims>
+struct ChainHelper<ListBase<T, dims> > {
+  template<class Container>
+  static void InsertDummies(Container* c) {
+    c->template Add<DummyList<T, dims> >(c->begin());
+    c->template Append<DummyList<T, dims> >();
+  }
+};
+
 }  // namespace detail
 
 template<class SublistType,
@@ -43,15 +66,20 @@ class List<
   SublistType,
   dims,
   kListOpVector,
-  detail::ChainedListVector<chain_dim, typename SublistType::DataType, dims,
+#define V_THIS_DATA_TYPE typename SublistType::DataType
+  detail::ChainedListVector<chain_dim, V_THIS_DATA_TYPE, dims,
                             SublistType>,
   typename SublistType::DataType,
-  typename std::enable_if<dims >= ListTraits<SublistType>::kDims>::type
-#define V_THIS_BASE ListBase<typename SublistType::DataType, dims>
-  > : public V_THIS_BASE {
+  typename std::enable_if<dims >= ListTraits<SublistType>::kDims>::type>
+    : public ListBase<V_THIS_DATA_TYPE, dims>,
+      protected detail::ChainHelper<SublistType> {
   static_assert(chain_dim < dims, "chain_dim < dims");
-  typedef V_THIS_BASE ListBaseType;
-#undef V_THIS_BASE
+#define V_CHAIN_FOR_LATERAL_DIM(dim, body)                              \
+  for (unsigned dim = 0; dim < chain_dim; ++dim) do body while (false); \
+  for (unsigned dim = chain_dim + 1; dim < dims; ++dim) do body while (false);
+
+  typedef ListBase<V_THIS_DATA_TYPE, dims> ListBaseType;
+#undef V_THIS_DATA_TYPE
   typedef typename SublistType::DataType DataType;
   typedef typename ListBaseType::SizeArray SizeArray;
   typedef detail::ChainedListVector<chain_dim, DataType, dims, SublistType>
@@ -65,13 +93,15 @@ class List<
 
   template<typename... Sizes>
   List(ListVector<SublistType>&& lists, DataType* default_value,
+       std::vector<SizeArray>&& nesting_offsets,
        typename std::conditional<1 + sizeof...(Sizes) == dims,
        size_t, Disabler>::type size,
        Sizes... sizes)
       : ListBaseType(size, sizes...),
-        default_value_(default_value),
         lists_(std::move(lists)),
-        nesting_offsets_(lists_.size()) {}
+        default_value_(default_value),
+        nesting_offsets_(InsertDummies(lists_, std::move(nesting_offsets))),
+        fwd_skip_list_(nesting_offsets_.size() - 1, &nesting_offsets_) {}
 
   List(ListVector<SublistType>&& lists,
        DataType* default_value = nullptr,
@@ -79,9 +109,10 @@ class List<
        const std::vector<GapEntry>& sorted_gaps = {})
       : lists_(std::move(lists)),
         default_value_(default_value),
-        nesting_offsets_(lists_.size()) {
+        nesting_offsets_(lists_.size()),
+        fwd_skip_list_(0) {
     for (const auto& entry : lateral_offsets)
-      SetLateralOffset(entry.first, entry.second);
+      SetLateralOffset(entry.second, &nesting_offsets_[entry.first]);
 
     // compute non-lateral nesting offsets
     size_t last_gap_index = 0;
@@ -103,7 +134,7 @@ class List<
     V_THIS_COMPUTE_NESTING_OFFSETS(lists_.size());
 #undef V_THIS_COMPUTE_NESTING_OFFSETS
 
-    ClearSizeArray(ListBaseType::offsets_);
+    ListBaseType::offsets_.fill(0);
 
     // compute non-lateral size
     auto& sizes_ = ListBaseType::sizes_;
@@ -115,10 +146,15 @@ class List<
         lists_.back().sizes()[chain_dim] +
         trailing_gap;
 
-    for (unsigned dim = 0; dim < chain_dim; ++dim)
-      ComputeLateralSize(lists_, nesting_offsets_, sizes_, dim);
-    for (unsigned dim = chain_dim + 1; dim < dims; ++dim)
-      ComputeLateralSize(lists_, nesting_offsets_, sizes_, dim);
+    V_CHAIN_FOR_LATERAL_DIM(dim, {
+        ComputeLateralSize(lists_, nesting_offsets_, sizes_, dim);
+            });
+
+    // TODO: resizing nesting_offsets_ twice and moving skip list isn't ideal,
+    // but it makes the initialization simple
+    InsertDummies(lists_, std::move(nesting_offsets_));
+    fwd_skip_list_ = decltype(fwd_skip_list_)(nesting_offsets_.size() - 1,
+                                              &nesting_offsets_);
   }
 
   void ShrinkToFirst() override {
@@ -127,8 +163,18 @@ class List<
   }
 
   DataType& get(const SizeArray& indexes) const override {
-    static DataType todo = DataType();
-    return todo;  // TODO: implement
+    auto nonlateral_pos = fwd_skip_list_.get(indexes[chain_dim]);
+    const auto& list = lists_[nonlateral_pos.first];
+    bool within = nonlateral_pos.second < list.sizes()[chain_dim];
+    SizeArray local_indexes(indexes);  // TODO: avoid copies using && argument
+    local_indexes[chain_dim] = nonlateral_pos.second;
+    const auto& offset = nesting_offsets_[nonlateral_pos.first];
+    V_CHAIN_FOR_LATERAL_DIM(dim, {
+        local_indexes[dim] -= offset[dim];
+        within &= (offset[dim] <= indexes[dim]) &
+            (indexes[dim] < offset[dim] + list.sizes()[dim]);
+      });
+    return within ? list.get(local_indexes) : *default_value_;
   }
 
   typename View<DataType>::Iterator iterator_begin() const override {
@@ -137,13 +183,21 @@ class List<
   }
 
   const SizeArray& nesting_offset(size_t index) const {
-    return nesting_offsets_[index];
+    return nesting_offsets_[++index];
   }
 
  private:
-  static void ClearSizeArray(typename List::SizeArray& a) {
-    std::fill(a.begin(), a.end(), typename List::SizeArray::size_type());
-  }
+  struct NonLateralBucketSizeGetter {
+    NonLateralBucketSizeGetter(const std::vector<List::SizeArray>* o) : o_(o) {}
+    NonLateralBucketSizeGetter() = default;
+    size_t operator()(size_t index) const {
+      return (*o_)[index + 1][chain_dim] - (*o_)[index][chain_dim];
+    }
+   private:
+    const std::vector<List::SizeArray>* o_;
+  };
+
+  typedef ImmutableSkipList<NonLateralBucketSizeGetter> SkipListType;
 
   static void ComputeLateralSize(const Container& lists,
                                  const std::vector<SizeArray>& nesting_offsets,
@@ -162,15 +216,39 @@ class List<
     size -= min_nesting_offset;
   }
 
-  void SetLateralOffset(size_t index, const LateralOffset& offset) {
-    auto& nesting_offset = nesting_offsets_[index];
-    std::copy(offset.begin() + chain_dim, offset.end(),
-              1+std::copy_n(offset.begin(), chain_dim, nesting_offset.begin()));
+  static void SetLateralOffset(const LateralOffset& offset,
+                               SizeArray* nesting_offset) {
+    using namespace std;
+    copy(offset.begin() + chain_dim, offset.end(),
+         1 + copy_n(offset.begin(), chain_dim, nesting_offset->begin()));
   }
+
+  static std::vector<SizeArray>&&
+  InsertDummies(Container& lists_, std::vector<SizeArray>&& nesting_offsets_) {
+    nesting_offsets_.emplace(nesting_offsets_.begin());
+    nesting_offsets_.front().fill(0);
+    if (lists_.empty())
+      nesting_offsets_.emplace_back();
+    else {
+      const auto& last_nesting_offset = nesting_offsets_.back();
+      nesting_offsets_.emplace_back();
+      nesting_offsets_.back()[chain_dim] = last_nesting_offset[chain_dim] +
+          lists_.back().sizes()[chain_dim];
+
+      detail::ChainHelper<SublistType>::template InsertDummies(&lists_);
+      lists_.Shrink();
+    }
+    SetLateralOffset({}, &nesting_offsets_.back());
+    nesting_offsets_.shrink_to_fit();
+    return std::move(nesting_offsets_);
+  }
+#undef V_CHAIN_FOR_LATERAL_DIM
 
   Container lists_;
   DataType* default_value_;
   std::vector<SizeArray> nesting_offsets_;
+
+  SkipListType fwd_skip_list_;
 };
 
 }  // namespace v
--- test/chain_test.cpp
+++ test/chain_test.cpp
@@ -20,7 +20,7 @@ TEST(ChainTest, PolyNoGapNoOffsets) {
           return digits + (4 + col) * ((!row && col < 2) || (row && col >= 2));
         }, 4, 2)
       .Append([&](unsigned col, unsigned row) {
-          return digits + (8 + (row + 1) / 2) * (col % 2);
+          return digits + (7 + (row + 1) / 2) * (row % 2);
         }, 1, 4),
       &digits[10]);  // use -1 as default
   typedef ListBaseType::SizeArray SizeArray;
@@ -28,6 +28,29 @@ TEST(ChainTest, PolyNoGapNoOffsets) {
   EXPECT_EQ((SizeArray({0, 0})), c2_0_nogap.nesting_offset(0));
   EXPECT_EQ((SizeArray({3, 0})), c2_0_nogap.nesting_offset(1));
   EXPECT_EQ((SizeArray({7, 0})), c2_0_nogap.nesting_offset(2));
+
+  // validate all non-zero entries and a few other corner cases
+  EXPECT_EQ(1, c2_0_nogap.get({0, 0}));
+  EXPECT_EQ(2, c2_0_nogap.get({1, 1}));
+  EXPECT_EQ(3, c2_0_nogap.get({2, 2}));
+  EXPECT_EQ(4, c2_0_nogap.get({3, 0}));
+  EXPECT_EQ(5, c2_0_nogap.get({4, 0}));
+  EXPECT_EQ(6, c2_0_nogap.get({5, 1}));
+  EXPECT_EQ(7, c2_0_nogap.get({6, 1}));
+  EXPECT_EQ(8, c2_0_nogap.get({7, 1}));
+  EXPECT_EQ(9, c2_0_nogap.get({7, 3}));
+  EXPECT_EQ(0, c2_0_nogap.get({3, 1}));
+  EXPECT_EQ(0, c2_0_nogap.get({7, 2}));
+  EXPECT_EQ(-1, c2_0_nogap.get({3, 2}));
+  EXPECT_EQ(-1, c2_0_nogap.get({0, 3}));
+  EXPECT_EQ(-1, c2_0_nogap.get({6, 2}));
+
+  // validate the number of zero entries is correct
+  unsigned zero_cnt = 0;
+  for (unsigned col = 0; col < c2_0_nogap.sizes()[0]; ++col)
+    for (unsigned row = 0; row < c2_0_nogap.sizes()[1]; ++row)
+      zero_cnt += c2_0_nogap.get({col, row}) == 0;
+  EXPECT_EQ(12, zero_cnt);
 }
 
 TEST(ChainTest, PolyConst) {
@@ -50,10 +73,10 @@ TEST(ChainTest, PolyConst) {
   //         +---+
   const int digits[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, -1};
   typedef ListBase<const int, 2> ListBaseType;
-  Chain<ListBaseType, 1> c2_1(
-      ListVector<ListBaseType>()
-      .Append([&](unsigned col, unsigned row) {
-          return digits + (1 + col) * (row + col == 2);
+  auto&& c2_1_lists_factory = [&]() {
+    return std::move(ListVector<ListBaseType>()
+     .Append([&](unsigned col, unsigned row) {
+          return digits + (1 + row) * (row + col == 2);
         }, 3, 3)
       .Append([&](unsigned col, unsigned row) {
           return digits + (4 + col) * (row == col % 2);
@@ -63,7 +86,10 @@ TEST(ChainTest, PolyConst) {
         }, 1, 2)
       .Append([&](unsigned col, unsigned row) {
           return digits + 9 * (row == 1 && col == 0);
-        }, 2, 2),
+        }, 2, 2));
+  };
+  Chain<ListBaseType, 1> c2_1(
+      c2_1_lists_factory(),
       &digits[10],  // use -1 as default
       {{0, {2}}, {2, {1}}, {3, {3}}},
       {{2, 1}});
@@ -73,6 +99,37 @@ TEST(ChainTest, PolyConst) {
   EXPECT_EQ((SizeArray({0, 3})), c2_1.nesting_offset(1));
   EXPECT_EQ((SizeArray({1, 6})), c2_1.nesting_offset(2));
   EXPECT_EQ((SizeArray({3, 8})), c2_1.nesting_offset(3));
+
+  EXPECT_EQ(1, c2_1.get({4, 0}));
+  EXPECT_EQ(2, c2_1.get({3, 1}));
+  EXPECT_EQ(3, c2_1.get({2, 2}));
+  EXPECT_EQ(4, c2_1.get({0, 3}));
+  EXPECT_EQ(5, c2_1.get({1, 4}));
+  EXPECT_EQ(6, c2_1.get({2, 3}));
+  EXPECT_EQ(7, c2_1.get({3, 4}));
+  EXPECT_EQ(8, c2_1.get({1, 7}));
+  EXPECT_EQ(9, c2_1.get({3, 9}));
+  EXPECT_EQ(0, c2_1.get({1, 3}));
+  EXPECT_EQ(0, c2_1.get({4, 9}));
+  EXPECT_EQ(-1, c2_1.get({0, 0}));
+  EXPECT_EQ(-1, c2_1.get({1, 2}));
+  EXPECT_EQ(-1, c2_1.get({1, 5}));
+
+  // verify construction via explicitly provided offsets
+  Chain<ListBaseType, 1> c2_1_explicit(
+      c2_1_lists_factory(),
+      &digits[10],
+      {{2, 0}, {0, 3}, {1, 6}, {3, 8}},
+      5, 10);
+  ASSERT_EQ((SizeArray({5, 10})), c2_1_explicit.sizes());
+  EXPECT_EQ((SizeArray({2, 0})), c2_1_explicit.nesting_offset(0));
+  EXPECT_EQ((SizeArray({0, 3})), c2_1_explicit.nesting_offset(1));
+  EXPECT_EQ((SizeArray({1, 6})), c2_1_explicit.nesting_offset(2));
+  EXPECT_EQ((SizeArray({3, 8})), c2_1_explicit.nesting_offset(3));
+  for (unsigned col = 0; col < c2_1.sizes()[0]; ++col)
+    for (unsigned row = 0; row < c2_1.sizes()[1]; ++row)
+      EXPECT_EQ(c2_1.get({col, row}), c2_1_explicit.get({col, row})) <<
+          "Incorrect get(" << col << ", " << row << ')';
 }
 
 TEST(ChainTest, Poly1DLeadingGap) {
@@ -88,6 +145,7 @@ TEST(ChainTest, Poly1DLeadingGap) {
       {{0, 3}});
   EXPECT_EQ(4, c1.sizes().front());
   EXPECT_EQ(3, c1.nesting_offset(0).front());
+  EXPECT_EQ("value", c1.get({3}));
 }
 
 TEST(ChainTest, PolyTrailingGap) {
@@ -104,4 +162,5 @@ TEST(ChainTest, PolyTrailingGap) {
   typedef ListBaseType::SizeArray SizeArray;
   EXPECT_EQ((SizeArray({1, 3, 1})), c3_1.sizes());
   EXPECT_EQ((SizeArray({0, 0, 0})), c3_1.nesting_offset(0));
+  EXPECT_EQ("value", c3_1.get({0, 0, 0}));
 }
