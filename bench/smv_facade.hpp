6104ba87ade04335376f234a3f284e5943afebe9
Leo Osvald <leo.osvald@gmail.com>
2014-08-19 23:56:25 -0400

    Implement iteration over nondefault values in STL map SMV facade.

--- bench/smv_facade.hpp
+++ bench/smv_facade.hpp
@@ -4,6 +4,7 @@
 #include "smv.hpp"
 
 #include <array>
+#include <iterator>
 #include <map>
 #include <utility>
 
@@ -45,9 +46,39 @@ class SmvFacadeMapHelper
   typedef typename SmvFacadeMapHelper::DataType DataType;
   typedef typename SmvFacadeMapHelper::CoordType CoordType;
 
+  class ValuesView {
+    typedef typename Map::iterator MapIter;
+   public:
+    // don't use iterator.hpp to avoid bias in benchmarks and be transparent
+    class Iterator : public std::iterator<std::forward_iterator_tag,
+                                          DataType,
+                                          typename MapIter::difference_type> {
+     public:
+      Iterator(MapIter it) : it_(it) {}
+      Iterator& operator++() { ++it_; return *this; }
+      Iterator& operator++(int) { Iterator old(it_); ++it_; return old; }
+      DataType& operator*() const { return it_->second; }
+      DataType* operator->() const { return &this->operator*(); }
+      bool operator==(const Iterator& other) const {
+        return this->it_ == other.it_;
+      }
+      bool operator!=(const Iterator& other) const { return !(*this == other); }
+     private:
+      MapIter it_;
+    };
+
+    ValuesView(Map* map) : map_(map) {}
+    Iterator begin() const { return map_->begin(); }
+    Iterator end() const { return map_->end(); }
+    size_t size() const { return map_->size(); }
+   private:
+    Map* map_;
+  };
+
   SmvFacadeMapHelper(DataType* default_value,
                      const CoordType& row_count, const CoordType& col_count)
-      : V_THIS_BASE_TYPE(default_value, row_count, col_count) {}
+      : V_THIS_BASE_TYPE(default_value, row_count, col_count),
+        values_(&map_) {}
 #undef V_THIS_BASE_TYPE
 
   DataType& operator()(const CoordType& row, const CoordType& col) {
@@ -61,6 +92,7 @@ class SmvFacadeMapHelper
 
  protected:
   mutable Map map_;
+  ValuesView values_;
 };
 
 }  // namespace detail
@@ -79,6 +111,8 @@ class SmvFacade<V_THIS_ADAPTEE_TYPE>
   SmvFacade(typename Helper::DataType* default_value,
             const CoordType& row_count, const CoordType& col_count)
       : Helper(default_value, row_count, col_count) {}
+
+  const typename SmvFacade::ValuesView& values() const { return this->values_; }
 };
 
 #endif  /* CPPVIEWS_BENCH_SMV_FACADE_HPP_ */
